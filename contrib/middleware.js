31231ed3b7ca7205501691fd535d3d3b2331e3ef
Eric Martindale <eric@ericmartindale.com>
2017-04-02 21:44:50 -0700

    Begin to add tests

--- contrib/middleware.js
+++ contrib/middleware.js
@@ -1,8 +1,9 @@
-var crypto = require('sha3');
-
 module.exports = function Hashcash (difficulty) {
+  var crypto = require('sha3');
   
   function isValid (input) {
+    console.log('is input valid?', input);
+
     var parts = input.split(':');
     var claim = parts[1];
     
@@ -24,11 +25,13 @@ module.exports = function Hashcash (difficulty) {
   }
   
   return function HashcashMiddleware (req, res, next) {
+    console.log('incoming hashcash:', req.headers);
+    
     var cash = req.headers['X-Hashcash'];
-    if (!cash) return res.error('Not enough hashcash.  Please try again.');
+    if (!cash) return res.status(500).send('Not enough hashcash.  Please try again.');
     
     var valid = isValid(cash);
-    if (!valid) return res.error('Insufficient difficulty.  Please try again.');
+    if (!valid) return res.status(500).send('Insufficient difficulty.  Please try again.');
     
     return next();
     
--- hashcasher.js
+++ hashcasher.js
@@ -73,5 +73,3 @@ function privateEventHandler(e) {
   
   return false;
 }
-
-
