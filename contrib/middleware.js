ae36896ab5c6fa83f3af03fa963fabe89806966c
Eric Martindale <eric@ericmartindale.com>
2017-11-09 13:05:53 -0800

    Simplify general flow

--- contrib/middleware.js
+++ contrib/middleware.js
@@ -1,9 +1,9 @@
 module.exports = function Hashcash (difficulty) {
   var crypto = require('sha3');
+
   
   function isValid (input) {
-    console.log('is input valid?', input);
-
+    var difficulty = 2;
     var parts = input.split(':');
     var claim = parts[1];
     
@@ -25,15 +25,12 @@ module.exports = function Hashcash (difficulty) {
   }
   
   return function HashcashMiddleware (req, res, next) {
-    console.log('incoming hashcash:', req.headers);
-    
-    var cash = req.headers['X-Hashcash'];
-    if (!cash) return res.status(500).send('Not enough hashcash.  Please try again.');
-    
+    var cash = req.headers['x-hashcash'];
     var valid = isValid(cash);
-    if (!valid) return res.status(500).send('Insufficient difficulty.  Please try again.');
-    
+
+    if (!cash) return res.status(402).send('Not enough hashcash.  Please try again.');
+    if (!valid) return res.status(402).send('Insufficient difficulty.  Please try again.');
+
     return next();
-    
   };
 }
